2295b6d96fb5ca16ac622a66193f15229eccd530
Jamshidbek_Karimov1 <jamshidbekarimov@gmail.com>
2024-09-16 20:42:56 +0500

    imports corrected

--- src/main/java/uz/jk/taskmanagement/domain/entity/TaskEntity.java
+++ src/main/java/uz/jk/taskmanagement/domain/entity/TaskEntity.java
@@ -2,9 +2,12 @@ package uz.jk.taskmanagement.domain.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import uz.jk.taskmanagement.domain.constants.TaskStatus;
-
 
 import java.time.LocalDate;
 
