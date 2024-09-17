5141f92f76c0b52df48415e0d6e63f1386dc9f8b
Jamshidbek_Karimov1 <jamshidbekarimov@gmail.com>
2024-09-17 17:45:17 +0500

    swagger configured

--- src/main/java/uz/jk/taskmanagement/TaskManagementApplication.java
+++ src/main/java/uz/jk/taskmanagement/TaskManagementApplication.java
@@ -1,11 +1,20 @@
 package uz.jk.taskmanagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Task Management API",
+                version = "1.0",
+                description = "API for managing tasks in the task management system"
+        )
+)
 public class TaskManagementApplication {
 
     public static void main(String[] args) {
--- src/main/java/uz/jk/taskmanagement/controller/TaskController.java
+++ src/main/java/uz/jk/taskmanagement/controller/TaskController.java
@@ -1,5 +1,7 @@
 package uz.jk.taskmanagement.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,27 +22,41 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/tasks")
 @RequiredArgsConstructor
+@Tag(name = "Task Management", description = "Operations for managing tasks")
 public class TaskController {
 
     private final TaskService taskService;
 
     @PostMapping
+    @Operation(
+            summary = "Create a Task",
+            description = "Creates a task from input. Required fields: 'title'. Date format: 'yyyy-MM-dd'."
+    )
     public TaskEntity create(@RequestBody TaskRequest taskRequest) {
         return taskService.save(taskRequest);
     }
 
 
     @GetMapping
+    @Operation(
+            summary = "Get all Tasks",
+            description = "Returns a list of all tasks. Supports pagination."
+    )
     public PageResponse<TaskEntity> findAll(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
         return taskService.findAll(page, size);
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Update a Task",
+            description = "Updates a task by ID. All fields are optional. Values for status: 'OPEN', 'IN_PROGRESS', 'DONE'."
+    )
     public TaskEntity update(@PathVariable UUID id, @RequestBody TaskRequest taskRequest) {
         return taskService.update(id, taskRequest);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a Task by id.")
     public void delete(@PathVariable UUID id) {
         taskService.deleteById(id);
     }
